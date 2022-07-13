9140282b10db286147476aaf4ecb48e3d57e719b
heart44 <gutt88656@gmail.com>
2022-07-13 17:44:23 +0900

    vue (07.13 8교시)

--- vue-router-pjt/src/views/EventClick.vue
+++ vue-router-pjt/src/views/EventClick.vue
@@ -13,15 +13,22 @@
                 <option>제주</option>
             </select>
         </div>
+        <div>
+            {{ txtVal }}            <!-- input에서 change 이벤트는 포커스를 벗어나야 실행됨 -->
+            <input type="txtVal" v-model="txtVal" @keyup="changeTxtVal" @keyup.enter="enterTxtVal">
+        </div>
     </div>
 </template>
-
+<!-- 이벤트 걸때 메소드에 -->
+<!-- 괄호가 없으면 호출해달라고 주소값만 보냄 -->
+<!-- 괄호가 있으면 실행해달라고 하는 것임(파라미터 보낼 수 있음) -->
 <script>
 export default {
     data() {
         return {
             selectedValue: '',
-            cnt: 0
+            cnt: 0,
+            txtVal: '',
         }
     },
     methods: {
@@ -30,6 +37,12 @@ export default {
         },
         printConsole() {
             console.log(this.cnt);
+        },
+        changeTxtVal() {
+            console.log(this.txtVal)
+        },
+        enterTxtVal() {
+            console.log('Enter');
         }
     }
 }
