2f75db66386e10ac5ef5e6beb9aa42df1fc0da4b
heart44 <gutt88656@gmail.com>
2022-07-13 13:10:32 +0900

    vue router (07.13 오전 수업)

---
+++ vue-router-pjt/src/views/IntroduceView.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <h1>나의 이름은 홍길동 입니다.</h1>
+        <input type="number" v-model="numberModel">
+        {{ printNumber }}
+        <div>
+            <label>여성<input type="radio" v-model.number="gender" value="0"></label>
+            <label>남성<input type="radio" v-model.number="gender" value="1"></label>
+        </div>
+        <div>{{ gender }}</div>
+        <div>{{ printGender }}</div>
+        <hr>
+        <div>
+            <h3>p.96</h3>
+            <div>{{ htmlString }}</div>
+            <div v-html="htmlString"></div>
+        </div>
+        <hr>
+        <div>
+            <h3>p.99</h3>
+            <textarea v-model="message"></textarea>
+        </div>
+        <hr>
+        <div>
+            <h3>p.100 - {{ city }}</h3>
+            <select v-model="city">
+                <option value="02">서울</option>
+                <option value="21">부산</option>
+                <option value="064">제주</option>
+            </select>
+        </div>
+        <hr>
+        <div>
+            <h3>p.101 - {{ basketball }}, {{ football }}</h3>
+            <label><input type="checkbox" v-model="basketball">농구</label>
+            <label><input type="checkbox" v-model="football" true-value="1" false-value="0">풋볼</label>
+        </div>
+        <hr>
+        <div>
+            <input type="text" v-model="textValue">
+            <br>
+            <input type="text" v-model="textValue2"><br>
+            <button type="button" v-bind:disabled="textValue === '' || textValue !== textValue2">전송</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numberModel: 3,
+            gender: 0,
+            htmlString: '<p style="color:red">This is a red string</p>',
+            message: 'textarea 입니다.',
+            city: '064',
+            basketball: true,
+            football: '1',
+            textValue: '',
+            textValue2: '',
+        }
+    },
+    //computed : watch랑 비슷, 연산 프로퍼티, 복잡한 로직 사용 시
+    //watch : 변수 값이 바뀌면 뭐 할래 이런 느낌
+    computed: {
+        printNumber() {
+            return this.numberModel + 3;
+        },
+        printGender() {
+            return this.gender === '0' ? '여성' : '남성';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
