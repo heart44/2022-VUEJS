5205d90d83dbf3310e7f7344ebb6f309c6e61021
heart44 <gutt88656@gmail.com>
2022-07-15 16:10:40 +0900

    vue shop 카카오 로그인 (07.15 6교시)

---
+++ vue-shop2-pjt/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app');
+
+window.Kakao.init('400971afcc07d18dede3bc9f981ac358');
---
+++ vue-shop2-pjt/src/views/KakaoLogin.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <a id="custom-login-btn" @click="kakaoLogin">
+            <img src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" width="222" alt="카카오 로그인 버튼"/>
+        </a>
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        kakaoLogin() {
+            window.Kakao.Auth.login({
+                scope: 'profile_nickname, profile_image, account_email',
+                success: this.getkakaoAccount,
+                fail: e => {
+                    console.error(e);
+                }
+            });
+        },
+        getkakaoAccount() {
+            window.Kakao.API.request({
+                url: '/v2/user/me',
+                success: res => {
+                    const acc = res.kakao_account;
+                    const params = {
+                        email: acc.email,
+                        nm: acc.profile.nickname,
+                        
+                    }
+
+                    alert('로그인 성공!');
+                },
+                fail: e => {
+                    console.error(e);
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
