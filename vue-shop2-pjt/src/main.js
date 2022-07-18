75eec5cec98f6db0d2ed434b8356c159399f43ba
heart44 <gutt88656@gmail.com>
2022-07-18 21:38:22 +0900

    vue shop 제품 등록 (07.18 보강3교시)

--- vue-shop2-pjt/src/main.js
+++ vue-shop2-pjt/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import mixins from './mixins';
 import router from './router'
 import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App).mixin(mixins).use(store).use(router).mount('#app');
+createApp(App).mixin(mixins).use(store).use(router).use(VueSweetalert2).mount('#app');
 
 window.Kakao.init('400971afcc07d18dede3bc9f981ac358');
